f1bf0faf2386450c14cc58076641f524a2495301
Min RK <benjaminrk@gmail.com>
2016-09-29 15:25:39 +0200

    robustify post-link scripts
    - don’t suppress *all* output, only normal output (should help diagnose when there are real errors)
    - use `PREFIX/bin/jupyter-nbextension` rather than `jupyter nbextension`, which can resolve outside the env (possible cause of the failure)

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,7 +1,7 @@
 # Check to make sure we can install the ipyparallel widget.
-("${PREFIX}/bin/jupyter" serverextension --version > /dev/null 2>&1) || exit 0
-("${PREFIX}/bin/jupyter" nbextension --version > /dev/null 2>&1) || exit 0
+("${PREFIX}/bin/jupyter-serverextension" --version > /dev/null 2>&1) || exit 0
+("${PREFIX}/bin/jupyter-nbextension" --version > /dev/null 2>&1) || exit 0
 
 # Install the ipyparallel widget.
-"${PREFIX}/bin/jupyter" nbextension disable --sys-prefix --py ipyparallel > /dev/null 2>&1
-"${PREFIX}/bin/jupyter" serverextension disable --sys-prefix --py ipyparallel > /dev/null 2>&1
+"${PREFIX}/bin/jupyter-nbextension" disable --sys-prefix --py ipyparallel --Application.log_level=ERROR > /dev/null
+"${PREFIX}/bin/jupyter-serverextension" disable --sys-prefix --py ipyparallel --Application.log_level=ERROR > /dev/null
